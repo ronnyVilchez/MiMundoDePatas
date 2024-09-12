31775f4d306ee606b0750a71b4a197a989e0ece5
Ronny <ronnyd.1994@gmail.com>
2024-09-12 17:40:04 -0500

    add pages and components and jsons4

--- src/Pages/PageAccs.jsx
+++ src/Pages/PageAccs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Layout } from '../Components/Layout'
-import { Accesories } from '../Components/accesories'
+import { Accesories } from '../Components/Accesories'
 
 export const PageAccs = () => {
   return (
