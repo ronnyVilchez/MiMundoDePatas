9163e69e5ce8572c27b4c4bec96c04ea91f87337
Ronny <ronnyd.1994@gmail.com>
2024-09-12 17:41:30 -0500

    add pages and components and jsons5

--- src/Pages/PageHealtf.jsx
+++ src/Pages/PageHealtf.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Layout } from '../Components/Layout'
-import { Health } from '../Components/health'
+import { Health } from '../Components/Health'
 
 export const PageHealtf = () => {
   return (
