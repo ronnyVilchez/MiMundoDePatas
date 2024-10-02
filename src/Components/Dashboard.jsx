66ad64cf3538bdbd63efd95e462df032910b341d
Ronny <ronnyd.1994@gmail.com>
2024-10-02 13:04:56 -0500

    footer ref

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -14,7 +14,7 @@ export const Dashboard = () => {
                     <h1 className='font-semibold text-[4vh] sm:text-[5vw]'>Mi Mundo de Patas</h1>
                     <h3 className='font-semibold sm:text-[2vw] text-[2vh]'>Mascotitas Pet Shop</h3>
                     <p className='px-3'>
-                    "Somos tu veterinaria de confianza, dedicada al bienestar de tus mascotas, con productos y servicios de alta calidad para garantizar su salud y felicidad. Ofrecemos un enfoque personalizado para cada una de tus mascotas, asegurando que reciban el cuidado y atención que merecen. Nuestro equipo de expertos está comprometido con la excelencia y la empatía, proporcionando un ambiente cálido y profesional para que tanto tú como tus mascotas se sientan seguros y bien cuidados."
+                        "Somos tu veterinaria de confianza, dedicada al bienestar de tus mascotas, con productos y servicios de alta calidad para garantizar su salud y felicidad. Ofrecemos un enfoque personalizado para cada una de tus mascotas, asegurando que reciban el cuidado y atención que merecen. Nuestro equipo de expertos está comprometido con la excelencia y la empatía, proporcionando un ambiente cálido y profesional para que tanto tú como tus mascotas se sientan seguros y bien cuidados."
                     </p>
                 </section>
 
@@ -26,7 +26,7 @@ export const Dashboard = () => {
             <section className='bg-[#fcb0dd] font-Poppins flex flex-col gap-8 text-center py-4'>
                 <h2 className='sm:text-[2vw] text-[2.7vh]  font-semibold'>NUESTROS PRODUCTOS</h2>
                 <p className='text-[2vh] sm:text-[1.5vw] '>
-                "Descubre nuestra selección de productos especializados para el cuidado y salud de tus mascotas. Ofrecemos alimentos, accesorios y medicinas de la mejor calidad."</p>
+                    "Descubre nuestra selección de productos especializados para el cuidado y salud de tus mascotas. Ofrecemos alimentos, accesorios y medicinas de la mejor calidad."</p>
                 <section className='flex flex-col sm:flex-row gap-8 justify-evenly items-center'>
 
                     <CardOpcion
@@ -78,7 +78,7 @@ export const Dashboard = () => {
                         spanDes={'Desparacitacion,etc'}
                         linktext={'Agendar'}
                     />
-                  
+
                 </section>
                 <div className=" h-[4px] bg-[#f88acc] w-[70%] sm:w-[90%] flex mx-auto"></div>
 
@@ -88,7 +88,7 @@ export const Dashboard = () => {
                 <h2 className='sm:text-[2vw] text-[2.7vh] font-semibold uppercase'>Staff de especialistas</h2>
                 <p className='text-[2vh] sm:text-[1.5vw]'>"Nuestro staff de especialistas está formado por profesionales experimentados, dedicados a ofrecer soluciones de alta calidad. Siempre enfocados en brindar el mejor servicio personalizado para cada cliente."</p>
                 <section className='flex flex-col sm:flex-row gap-10 justify-evenly items-center'>
-              
+
                     <section className='flex flex-col items-center gap-3 px-4'>
                         <img className='rounded-xl' src="/images/staff/dr1.jpg" alt="img" width={200} />
                         <h2 className='font-semibold'>Dr. Sarah Smith</h2>
@@ -113,34 +113,33 @@ export const Dashboard = () => {
 
             <footer className='bg-[#f88acc] font-Poppins flex flex-col gap-4 sm:gap-0 sm:flex-row p-2 sm:justify-around w-full sm:items-center'>
 
-                <section className='flex flex-col sm:w-1/2 '>
-                    <h2>SIGUENOS EN TODAS NUESTRAS REDES</h2>
+                <section className='flex flex-col gap-4 sm:w-[45%] '>
+                    <h2 className='text-center font-semibold'>SIGUENOS EN TODAS NUESTRAS REDES</h2>
                     <section className='flex flex-row justify-evenly'>
-                        <Link>
+                        <a target='_blank' href={'https://www.fb.com'}>
                             <img src="/images/Facebook.svg" alt="Facebook" width={50} />
-                        </Link>
+                        </a>
 
-                        <Link>
+                        <a target='_blank' href={'https://www.x.com'}>
                             <img src="/images/Twitter.svg" alt="Twitter" width={50} />
-                        </Link>
+                        </a>
 
-                        <Link>
+                        <a target='_blank' href={'https://www.instagram.com'}>
                             <img src="/images/insta.svg" alt="insta" width={50} />
-                        </Link>
+                        </a>
 
-                        <Link>
+                        <a target='_blank' href={'https://www.tiktok.com'}>
                             <img src="/images/tiktok.svg" alt="tiktok" width={50} />
-                        </Link>
+                        </a>
                     </section>
-
 
                 </section>
 
-                <section className='flex flex-col sm:w-1/2 gap-4'>
-                    <h2>VISITANOS EN:</h2>
+                <div className=" w-[5px] bg-[#e256b4] sm:h-[5rem] flex mx-auto"></div>
+              
+                <section className='flex flex-col sm:w-[45%] gap-4'>
+                    <h2 className='text-center font-semibold'>VISITANOS EN:</h2>
                     <MapPrimavera />
-                    {/*  <Link  to={'https://maps.app.goo.gl/aAT3uUZjEqzJaZ1h7'}>La Primavera: Eleodoro Demetrio Coral N°125, Chiclayo.</Link> */}
-                    {/*  <Link to={'https://maps.app.goo.gl/KQpZzyq2QPR2YYH96'}>Mochoqueque: Avenida Kennedy & Palmeras, Chiclayo.</Link> */}
                     <MapaDorado />
 
                 </section>
